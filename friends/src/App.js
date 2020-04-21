3110cc3c2dcac54a8ce55739d8a591b342e3df71
Henry Nelson <Henry.Nelson89@gmail.com>
2020-04-21 12:56:32 -0700

    I created the log in form

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-
+import Login from './Components/login'
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <switch>
+        <Switch>
           <Route path= "/login" component={Login} />
-        </switch>
+          <Route component={Login} />
+        </Switch>
 
       </div>
     </Router>
--- friends/src/Components/login.js
+++ friends/src/Components/login.js
@@ -7,5 +7,34 @@ class Login extends React.Component {
             password: ""
         }
     };
-    
+
+
+    render() {
+        return (
+            <div className="formDiv">
+                <form>
+                    <label htmlFor= "username" name= "username" >Username*</label> 
+                    <br/>
+
+                    <input
+                    type= "text"
+                    name= "username"
+                    value= {this.state.credentials.username}
+                    />
+                    <br/>
+                    <label htmlFor= "password" name= "password" >Password*</label> 
+                    <br/>
+                    <input
+                    type= "text"
+                    name= "password"
+                    value= {this.state.credentials.password}
+                    />
+                    <br/>
+                    <button>Log In</button>
+                </form>
+            </div>
+        )
+    }
 }
+
+export default Login
